b7407d3bc030940e84da946505e3c63285577ed8
rosalesponcer <44475922+rosalesponcer@users.noreply.github.com>
2023-08-30 11:24:37 -0500

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -3,7 +3,7 @@ mkdir /etc/nginx/html/huddle-landing-page-with-curved-sections-master
 
 mkdir /etc/nginx/html/huddle-landing-page-with-curved-sections-master/images /etc/nginx/html/huddle-landing-page-with-curved-sections-master/dist /etc/nginx/html/huddle-landing-page-with-curved-sections-master/dist/css
 
-cp -rf ./images/* /etc/nginx/html/huddle-landing-page-with-curved-sections-master/images/
-cp -rf ./dist/css/* /etc/nginx/html/huddle-landing-page-with-curved-sections-master/dist/css/
-cp -rf ./app.js /etc/nginx/html/huddle-landing-page-with-curved-sections-master/
-cp -rf ./index.html /etc/nginx/html/huddle-landing-page-with-curved-sections-master/
+mv ./images/* /etc/nginx/html/huddle-landing-page-with-curved-sections-master/images/
+mv ./dist/css/* /etc/nginx/html/huddle-landing-page-with-curved-sections-master/dist/css/
+mv ./app.js /etc/nginx/html/huddle-landing-page-with-curved-sections-master/
+mv ./index.html /etc/nginx/html/huddle-landing-page-with-curved-sections-master/

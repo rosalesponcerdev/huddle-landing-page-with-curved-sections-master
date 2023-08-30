b40849798d49b97a1bd978abb27b4b8b898caa97
rosalesponcer <44475922+rosalesponcer@users.noreply.github.com>
2023-08-30 11:25:24 -0500

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,9 +1,9 @@
-rm -r /etc/nginx/html/huddle-landing-page-with-curved-sections-master/
-mkdir /etc/nginx/html/huddle-landing-page-with-curved-sections-master
+rm -r /var/www/briefcase/huddle-landing-page-with-curved-sections-master/
+mkdir /var/www/briefcase/huddle-landing-page-with-curved-sections-master
 
-mkdir /etc/nginx/html/huddle-landing-page-with-curved-sections-master/images /etc/nginx/html/huddle-landing-page-with-curved-sections-master/dist /etc/nginx/html/huddle-landing-page-with-curved-sections-master/dist/css
+mkdir /var/www/briefcase/huddle-landing-page-with-curved-sections-master/images /var/www/briefcase/huddle-landing-page-with-curved-sections-master/dist /var/www/briefcase/huddle-landing-page-with-curved-sections-master/dist/css
 
-mv ./images/* /etc/nginx/html/huddle-landing-page-with-curved-sections-master/images/
-mv ./dist/css/* /etc/nginx/html/huddle-landing-page-with-curved-sections-master/dist/css/
-mv ./app.js /etc/nginx/html/huddle-landing-page-with-curved-sections-master/
-mv ./index.html /etc/nginx/html/huddle-landing-page-with-curved-sections-master/
+mv ./images/* /var/www/briefcase/huddle-landing-page-with-curved-sections-master/images/
+mv ./dist/css/* /var/www/briefcase/huddle-landing-page-with-curved-sections-master/dist/css/
+mv ./app.js /var/www/briefcase/huddle-landing-page-with-curved-sections-master/
+mv ./index.html /var/www/briefcase/huddle-landing-page-with-curved-sections-master/

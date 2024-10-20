e378698acbd4c8479a79d3a05c68d7a7ebc46baf
Renzo Rosales <renzoalfredo.rosalesponce@emeal.nttdata.com>
2024-10-20 12:05:00 -0500

    ci: Implementar docker action

--- copy.sh
+++
@@ -1,9 +0,0 @@
-rm -r /var/www/briefcase/huddle-landing-page-with-curved-sections-master/
-mkdir /var/www/briefcase/huddle-landing-page-with-curved-sections-master
-
-mkdir /var/www/briefcase/huddle-landing-page-with-curved-sections-master/images /var/www/briefcase/huddle-landing-page-with-curved-sections-master/dist /var/www/briefcase/huddle-landing-page-with-curved-sections-master/dist/css
-
-mv ./images/* /var/www/briefcase/huddle-landing-page-with-curved-sections-master/images/
-mv ./dist/css/* /var/www/briefcase/huddle-landing-page-with-curved-sections-master/dist/css/
-mv ./app.js /var/www/briefcase/huddle-landing-page-with-curved-sections-master/
-mv ./index.html /var/www/briefcase/huddle-landing-page-with-curved-sections-master/
